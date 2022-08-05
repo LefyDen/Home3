f2a452317fe4d4e6f3c329cc917692a1bda9e1b9
LefyDen <denisyuk.denis.s@gmail.com>
2022-08-05 18:54:53 +0300

    inital version

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HOME3
+{
+    class Program
+    {
+        static bool Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            while (x <= y)
+            {               
+                sum = sum +x;
+                x++;
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
